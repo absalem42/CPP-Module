76faea045fb0fcc51c65611502fd8700207abd04
absalem42 <128146228+absalem42@users.noreply.github.com>
2024-03-18 13:27:07 +0000

    finish ex00

---
+++ cpp-02/ex00/Fixed.cpp
@@ -0,0 +1,40 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed() : fixedPoint(0)
+{
+    std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed& copy)
+{
+    std::cout << "Copy constructor called" << std::endl;
+    fixedPoint = copy.fixedPoint;
+}
+
+Fixed& Fixed::operator=(const Fixed& copy)
+{
+    std::cout << "Copy assignment operator called" << std::endl;
+    if (this != &copy)
+    {
+        fixedPoint = copy.fixedPoint;
+    }
+    return (*this);
+}
+
+Fixed::~Fixed()
+{
+    std::cout << "Destructor called" << std::endl;
+}
+
+
+int Fixed::getRawBits(void) const
+{
+    std::cout << "getRawBits member function called" << std::endl;
+    return(fixedPoint);
+}
+
+void Fixed::setRawBits(int const raw)
+{
+    std::cout << "setRawBits member function called" << std::endl;
+    fixedPoint = raw;
+}
---
+++ cpp-02/ex00/Fixed.hpp
@@ -0,0 +1,25 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+
+class Fixed{
+private:
+    int fixedPoint;
+    static const int numBits = 8;
+public:
+    Fixed();  // Default constructor
+    Fixed(const Fixed& copy);  // Copy constructor
+    Fixed& operator=(const Fixed& copy);    // Copy assignment operator
+    ~Fixed();  // Destructor
+
+    int getRawBits(void) const;
+    void setRawBits(int const raw);
+};
+
+
+
+
+
+
+#endif
